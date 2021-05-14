36d75b3adc83890fa1dd132b4d87a6566ba056e7
Yu-Chun Tung <dyj0183@byui.edu>
2021-05-14 09:09:20 -0600

    cleaned up codes

--- MegaDesk - Yu Chun/FileService.cs
+++ MegaDesk - Yu Chun/FileService.cs
@@ -11,19 +11,18 @@ namespace MegaDesk___Yu_Chun
 {
     public class FileService
     {
-  
-
         // constants
         public const string path = @"C:\Users\dyj0183\source\repos\MegaDesk - Yu Chun\MegaDesk - Yu Chun\bin\Debug\quotes.json";
         private List<DeskQuote> _quotes;
 
-        // methods
+        // constructor, as soon as the FileService is created, load all the data into _quotes
         public FileService()
         {
             LoadAllData();
-            
         }
 
+        // methods
+        // get all the quotes from quotes.json file, this is like a getter in this class
         public List<DeskQuote> GetQuotes()
         {
             if (_quotes == null)
@@ -34,40 +33,39 @@ namespace MegaDesk___Yu_Chun
             return _quotes;
         }
 
+        // add a new quote to the quote list, this is like a setter in this class
         public void AddQuote(DeskQuote deskQuote)
         {
             _quotes.Add(deskQuote);
-            SaveAllData();
+            SaveAllData(); 
         }
 
+        // retrieve all the data from quotes.json file, use "private" to protect this method
         private void LoadAllData()
         {
             if (File.Exists(path))
             {
-                string data = File.ReadAllText(path);
-                _quotes = JsonSerializer.Deserialize<List<DeskQuote>>(data);
+                string data = File.ReadAllText(path); // data will be in json format
+                _quotes = JsonSerializer.Deserialize<List<DeskQuote>>(data); // convert from json to List<DeskQuote>
             } 
             else
             {
+                // if the file doesn't even exist, just create a new empty list
                 _quotes = new List<DeskQuote>();
-
             }
         }
+
+        // write all the data into the quotes.json file, use "private" to protect this method
         private void SaveAllData()
         {
+            // convert the _quotes which is List<DeskQuote> format to json format
             string myJson = JsonSerializer.Serialize(_quotes);
 
-          
-             // create a file to write to
-             File.WriteAllText(path, myJson);
-            
-
-          
-
+            // create a file and write the json data into it
+            File.WriteAllText(path, myJson);
+           
+            // console out to see the result when testing
             Console.WriteLine(myJson);
-
         }
-
-
     }
 }
